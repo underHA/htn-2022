338c926ace32943d57219d89f17fc8da6bb69ebb
Lincoln <Lsha0730@gmail.com>
2022-09-17 19:26:53 -0700

    Fix dependency issues, add basic video feature

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Home from "./home/Home.js"
 import Watch from "./watch/Watch.js"
 import { siteContext } from "./Context.js"
+import MockCards from "./watch/MockCards";
 
 function App() {
   const [page, setPage] = useState("home")
   const [settings, setSettings] = useState({ closedCaptioning: true })
 
+  const caption = "This is, what we in the industry call a certified hood classic. This however, is lorem ipsum dolor sit amet is placeholder text"
+
   return (
     <siteContext.Provider value={{page, setPage, settings, setSettings}}>
       <div className="App">
-        {page == "loading"? <></> : (page == "home"? <Home />:<Watch />)}
+        {page == "loading"? <></> : (page == "home"? <Home />:<Watch historyCards={MockCards} caption={caption} />)}
       </div>
     </siteContext.Provider>
   );
--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -4,13 +4,14 @@ import { BsFillCloudMoonFill, BsArrowRight } from "react-icons/bs"
 import Dropdown from "../common/Dropdown"
 import TextField from "../common/TextField"
 import { siteContext } from "../Context"
+import backdrop from "./backdrop.png"
 
 const Home = (props) => {
     const {page, setPage} = useContext(siteContext)
 
     return (
         <div className="home-container">
-            <img className="background-image" alt="" src="https://static01.nyt.com/images/2022/09/01/business/00roose-1/merlin_212276709_3104aef5-3dc4-4288-bb44-9e5624db0b37-superJumbo.jpg"/>
+            <img className="background-image" alt="" src={backdrop} style={{width: "100%"}}/>
             {/* <iframe title="bruh" width={100} height={100} src={`${"https://www.youtube.com/watch?v=r-mAyu5RruU"}?autoplay=1`}></iframe> */}
             {/* <iframe width="200" height="100" src={`${"https://www.youtube.com/embed/r-mAyu5RruU"}?autoplay=1`} frameborder="0" allow="autoplay;" allowfullscreen></iframe> */}
             <div className="left-UI">
