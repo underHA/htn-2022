5da8cf2c40fbeaefef7c5e6c983f8665359f7bea
Hanmin Kim <hanminkim123@gmail.com>
2022-09-17 20:47:42 -0400

    connect gateways to main

--- server/main.py
+++ server/main.py
@@ -1,9 +1,8 @@
-import asyncio
-
 from flask import Flask, request
 
-from openai_gateway import get_image_prompt
+from openai_gateway import get_image_prompt, get_audio_prompt
 from server.banana_gateway import create_image
+from server.youtube_gateway import find_video
 
 app = Flask(__name__)
 
@@ -13,14 +12,18 @@ def hello():
     return "Hello World!"
 
 
+@app.route("/api/get_audio", methods=["POST"])
+async def get_audio():
+    mood_word = await get_audio_prompt(request.json["prompt"])
+    return await find_video(mood_word)
+
+
 @app.route("/api/get_video", methods=['POST'])
-def get_prompt():
+async def get_prompt():
     prompt = request.json['prompt']
+    style = request.json['style']
 
-    create_image([get_image_prompt(prompt)])
-
-
-
+    return await create_image([get_image_prompt(prompt, style)])
 
 
 if __name__ == "__main__":
